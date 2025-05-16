b40cc40928ac05bfcc89974a52714785dd19c9e3
theowilliams <theowilliams@uiowa.edu>
2025-05-15 19:01:46 -0500

    commit 2

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template, request, redirect
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
+import json
+import os
 
 app = Flask(__name__)
 
 # Google Sheets setup
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+# Load credentials from environment variable
+creds_dict = json.loads(os.environ['GOOGLE_CREDENTIALS_JSON'])
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 sheet = client.open('Song Suggestions').sheet1  # or use .worksheet('Sheet1') if needed
 
